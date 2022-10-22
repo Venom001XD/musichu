ec6e4944d6c10fc290870d6832e484b015ed5945
NoobStark-1 <115385229+NoobStark-1@users.noreply.github.com>
2022-10-22 08:43:37 +0530

    Update song.py

--- VIVI/utils/inline/song.py
+++ VIVI/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🔷 sᴜᴩᴩᴏʀᴛ 🔷", url=f"{config.SUPPORT_GROUP}",
+                text="sᴜᴩᴩᴏʀᴛ", url=f"{config.SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"

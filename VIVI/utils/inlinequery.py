8bbc53886aad831c89beb2a26f2599bfc72033b2
NoobStark-1 <115385229+NoobStark-1@users.noreply.github.com>
2022-10-26 08:46:13 +0530

    Update and rename inlinequery.txt to inlinequery.py

--- VIVI/utils/inlinequery.py
+++ VIVI/utils/inlinequery.py
@@ -6,37 +6,37 @@ answer = []
 answer.extend(
     [
         InlineQueryResultArticle(
-            title="🔷 ᴩᴀᴜsᴇ ",
+            title="ᴩᴀᴜsᴇ ",
             description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
             thumb_url="https://telegra.ph/file/b95e95e731d9c33bc4bc5.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
-            title="🔷 ʀᴇsᴜᴍᴇ ",
+            title="ʀᴇsᴜᴍᴇ ",
             description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
             thumb_url="https://telegra.ph/file/b95e95e731d9c33bc4bc5.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
-            title="🔷 sᴋɪᴩ ",
+            title="sᴋɪᴩ ",
             description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
             thumb_url="https://telegra.ph/file/b95e95e731d9c33bc4bc5.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
-            title="🔷 ᴇɴᴅ ",
+            title="ᴇɴᴅ ",
             description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
             thumb_url="https://telegra.ph/file/b95e95e731d9c33bc4bc5.jpg",
             input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
-            title="🔷 sʜᴜғғʟᴇ ",
+            title="sʜᴜғғʟᴇ ",
             description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
             thumb_url="https://telegra.ph/file/b95e95e731d9c33bc4bc5.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
-            title="🔷 ʟᴏᴏᴩ ",
+            title="ʟᴏᴏᴩ ",
             description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
             thumb_url="https://telegra.ph/file/b95e95e731d9c33bc4bc5.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
